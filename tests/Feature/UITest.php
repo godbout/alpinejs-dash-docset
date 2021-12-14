85b40911eb0858ebfe6c4f35dad2838c09989c2b
Guill Lo <guill.bout@gmail.com>
2021-12-14 17:06:33 +0800

    add UI Test file

---
+++ tests/Feature/UITest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Docsets\Alpinejs;
+use Godbout\DashDocsetBuilder\Services\DocsetBuilder;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+use Wa72\HtmlPageDom\HtmlPageCrawler;
+
+class UITest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->docset = new Alpinejs();
+        $this->builder = new DocsetBuilder($this->docset);
+
+        if (! Storage::exists($this->docset->downloadedDirectory())) {
+            fwrite(STDOUT, PHP_EOL . PHP_EOL . "\e[1;33mGrabbing alpinejs..." . PHP_EOL);
+            Artisan::call('grab alpinejs');
+        }
+
+        if (! Storage::exists($this->docset->file())) {
+            fwrite(STDOUT, PHP_EOL . PHP_EOL . "\e[1;33mPackaging alpinejs..." . PHP_EOL);
+            Artisan::call('package alpinejs');
+        }
+    }
+
+    /** @test */
+    public function it_has_a_table_of_contents()
+    {
+        Config::set(
+            'database.connections.sqlite.database',
+            "storage/{$this->docset->databaseFile()}"
+        );
+
+        $this->assertNotEquals(0, DB::table('searchIndex')->count());
+    }
+
+    /** @test */
+    public function the_header_gets_removed_from_the_dash_docset_files()
+    {
+        $header = '<header';
+
+        $this->assertStringContainsString(
+            $header,
+            Storage::get($this->docset->downloadedIndex())
+        );
+
+        $this->assertStringNotContainsString(
+            $header,
+            Storage::get($this->docset->innerIndex())
+        );
+    }
+
+    /** @test */
+    public function the_left_sidebar_gets_removed_from_the_dash_docset_files()
+    {
+        $leftSidebar = 'fixed pb-6 pt-8 px-8 left-0 bottom-0 hidden md:block w-48 lg:w-64';
+
+        $this->assertStringContainsString(
+            $leftSidebar,
+            Storage::get($this->docset->downloadedIndex())
+        );
+
+        $this->assertStringNotContainsString(
+            $leftSidebar,
+            Storage::get($this->docset->innerIndex())
+        );
+    }
+
+    /** @test */
+    public function the_right_sidebar_gets_removed_from_the_dash_docset_files()
+    {
+        $rightSidebar = 'fixed py-8 px-6 right-0 bottom-0 w-64 overflow-y-auto hidden xl:block';
+
+        $this->assertStringContainsString(
+            $rightSidebar,
+            Storage::get($this->docset->downloadedIndex())
+        );
+
+        $this->assertStringNotContainsString(
+            $rightSidebar,
+            Storage::get($this->docset->innerIndex())
+        );
+    }
+
+    /** @test */
+    public function it_inserts_dash_anchors_in_the_doc_files()
+    {
+        $this->assertStringContainsString(
+            'name="//apple_ref/',
+            Storage::get($this->docset->innerIndex())
+        );
+    }
+}
